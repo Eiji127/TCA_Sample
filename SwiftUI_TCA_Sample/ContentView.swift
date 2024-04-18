12b3f1edaa2ce6f78f66c0042ae8ad1615bab6f1
Eiji <64912886+Eiji127@users.noreply.github.com>
2024-04-19 08:46:31 +0900

    [Add]: first commit

---
+++ SwiftUI_TCA_Sample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI_TCA_Sample
+//
+//  Created by 白数叡司 on 2024/03/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI_TCA_Sample/SwiftUI_TCA_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TCA_SampleApp.swift
+//  SwiftUI_TCA_Sample
+//
+//  Created by 白数叡司 on 2024/03/10.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TCA_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
