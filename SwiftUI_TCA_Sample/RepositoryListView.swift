f54ca3a1d04a4245b5cb4c8abe533a8ba2c13c1a
Eiji <64912886+Eiji127@users.noreply.github.com>
2024-04-19 09:01:34 +0900

    [Add]: add RepositoryListView.swift

---
+++ SwiftUI_TCA_Sample/RepositoryListView.swift
@@ -0,0 +1,18 @@
+//
+//  RepositoryListView.swift
+//  SwiftUI_TCA_Sample
+//
+//  Created by 白数叡司 on 2024/04/19.
+//
+
+import SwiftUI
+
+struct RepositoryListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RepositoryListView()
+}
