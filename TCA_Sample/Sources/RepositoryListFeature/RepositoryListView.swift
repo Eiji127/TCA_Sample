7630b2635bbbd63946b92596f3c1d9ad6a021184
Eiji <64912886+Eiji127@users.noreply.github.com>
2024-04-21 01:08:25 +0900

    BodyをRepositoryListに追加

--- TCA_Sample/Sources/RepositoryListFeature/RepositoryListView.swift
+++ TCA_Sample/Sources/RepositoryListFeature/RepositoryListView.swift
@@ -27,6 +27,52 @@ public struct RepositoryList {
     }
     
     public init() {}
+    
+    public var body: some ReducerOf<Self> {
+        Reduce { state, action in
+            switch action {
+            case .onAppear:
+                state.isLoading = true
+                return .run { send in
+                    await send(
+                        .searchRepositoriesResponse(
+                            Result {
+                                let query = "composable"
+                                let url = URL(string: "https://api.github.com/search/repositories?q=\(query)&sort=stars")!
+                                var request = URLRequest(url: url)
+                                if let token = Bundle.main.infoDictionary?["GitHubPersonalAccessToken"] as? String {
+                                    request.setValue(
+                                        "Bearer \(token)",
+                                        forHTTPHeaderField: "Authorization"
+                                    )
+                                }
+                                let (data, _) = try await URLSession.shared.data(for: request)
+                                let repositories = try jsonDecoder.decode(GithubSearchResult.self, from: data).items
+                                return repositories
+                            }
+                        )
+                    )
+                }
+            case let .searchRepositoriesResponse(result):
+                state.isLoading = false
+                
+                switch result {
+                case let .success(response):
+                    state.repositories = response
+                    return .none
+                case .failure:
+                    // TODO: Handling Error
+                    return .none
+                }
+            }
+        }
+    }
+    
+    private var jsonDecoder: JSONDecoder {
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        return decoder
+    }
 }
 
 public struct RepositoryListView: View {
