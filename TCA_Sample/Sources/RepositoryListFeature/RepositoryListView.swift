82adf3072e1de86b24534f9b3d6e292a97ab6c4a
Eiji <64912886+Eiji127@users.noreply.github.com>
2024-04-21 02:17:47 +0900

    Storeを用いてRepositoryListViewを構築

--- TCA_Sample/Sources/RepositoryListFeature/RepositoryListView.swift
+++ TCA_Sample/Sources/RepositoryListFeature/RepositoryListView.swift
@@ -7,8 +7,10 @@
 
 import ComposableArchitecture
 import Entity
+import Foundation
 import SwiftUI
 
+// MARK: - Reducer
 @Reducer
 public struct RepositoryList {
     @ObservableState
@@ -75,15 +77,65 @@ public struct RepositoryList {
     }
 }
 
+// MARK: - View
 public struct RepositoryListView: View {
+    let store: StoreOf<RepositoryList>
     
-    public init() {}
+    public init(store: StoreOf<RepositoryList>) {
+        self.store = store
+    }
     
     public var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Group {
+            if store.isLoading {
+                ProgressView()
+            } else {
+                List {
+                    ForEach(store.repositories, id: \.id) { repository in
+                        Button {
+                        } label: {
+                            VStack(alignment: .leading, spacing: 8) {
+                                Text(repository.fullName)
+                                    .font(.title2.bold())
+                                Text(repository.description ?? "")
+                                    .font(.body)
+                                    .lineLimit(2)
+                                HStack(alignment: .center, spacing: 32) {
+                                    Label(
+                                        title: {
+                                            Text("\(repository.stargazersCount)")
+                                                .font(.callout)
+                                        },
+                                        icon: {
+                                            Image(systemName: "star.fill")
+                                                .foregroundStyle(.yellow)
+                                        }
+                                    )
+                                    Label(
+                                        title: {
+                                            Text(repository.language ?? "")
+                                                .font(.callout)
+                                        },
+                                        icon: {
+                                            Image(systemName: "text.word.spacing")
+                                                .foregroundStyle(.gray)
+                                        }
+                                    )
+                                }
+                            }
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                        }
+                        .buttonStyle(.plain)
+                    }
+                }
+            }
+        }
+        .onAppear {
+            store.send(.onAppear)
+        } 
     }
 }
 
-#Preview {
-    RepositoryListView()
-}
+//#Preview {
+//    RepositoryListView()
+//}
