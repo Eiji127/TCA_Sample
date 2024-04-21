dd55f785113166dd43f60133c136e5e5495f4793
Eiji <64912886+Eiji127@users.noreply.github.com>
2024-04-22 00:50:22 +0900

    RepositoryRowViewを実装

--- TCA_Sample/Sources/RepositoryListFeature/RepositoryRowView.swift
+++ TCA_Sample/Sources/RepositoryListFeature/RepositoryRowView.swift
@@ -35,14 +35,60 @@ public struct RepositoryRow {
 
 // MARK: - View
 public struct RepositoryRowView: View {
+    let store: StoreOf<RepositoryRow>
     
-    public init() {}
+    public init(store: StoreOf<RepositoryRow>) {
+        self.store = store
+    }
     
     public var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button {
+            store.send(.rowTapped)
+        } label: {
+            VStack(alignment: .leading, spacing: 8) {
+                Text(store.repository.fullName)
+                    .font(.title2.bold())
+                Text(store.repository.description ?? "")
+                    .font(.body)
+                    .lineLimit(2)
+                HStack(alignment: .center, spacing: 32) {
+                    Label(
+                        title: {
+                            Text("\(store.repository.stargazersCount)")
+                                .font(.callout)
+                        },
+                        icon: {
+                            Image(systemName: "star.fill")
+                                .foregroundStyle(.yellow)
+                        }
+                    )
+                    Label(
+                        title: {
+                            Text(store.repository.language ?? "")
+                                .font(.callout)
+                        },
+                        icon: {
+                            Image(systemName: "text.word.spacing")
+                                .foregroundStyle(.gray)
+                        }
+                    )
+                }
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+        }
+        .buttonStyle(.plain)
     }
 }
 
 #Preview {
-    RepositoryRowView()
+    RepositoryRowView(
+        store: .init(
+            initialState: RepositoryRow.State(
+                repository: .mock(id: 1)
+            )
+        ) {
+            RepositoryRow()
+        }
+    )
+    .padding()
 }
