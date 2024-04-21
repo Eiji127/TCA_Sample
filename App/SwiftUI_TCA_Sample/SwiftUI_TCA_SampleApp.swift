3b9b7b4826b594576a0e61820d4323f0cc06bb3a
Eiji <64912886+Eiji127@users.noreply.github.com>
2024-04-22 00:00:51 +0900

    アプリのエントリポイントをRepositoryListViewとなるように設定

--- App/SwiftUI_TCA_Sample/SwiftUI_TCA_SampleApp.swift
+++ App/SwiftUI_TCA_Sample/SwiftUI_TCA_SampleApp.swift
@@ -5,14 +5,20 @@
 //  Created by 白数叡司 on 2024/03/10.
 //
 
-import SwiftUI
 import RepositoryListFeature
+import SwiftUI
 
 @main
 struct SwiftUI_TCA_SampleApp: App {
     var body: some Scene {
         WindowGroup {
-            EmptyView()
+            RepositoryListView(
+                store: .init(
+                    initialState: RepositoryList.State()
+                ) {
+                    RepositoryList()._printChanges()
+                }
+            )
         }
     }
 }
