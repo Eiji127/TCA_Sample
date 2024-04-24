ad27319872219dc09194406812fb1cc20a9664a0
Eiji <64912886+Eiji127@users.noreply.github.com>
2024-04-24 11:14:52 +0900

    RepositoryListViewに検索バーを追加

--- TCA_Sample/Sources/RepositoryListFeature/RepositoryListView.swift
+++ TCA_Sample/Sources/RepositoryListFeature/RepositoryListView.swift
@@ -95,30 +95,38 @@ public struct RepositoryList {
 
 // MARK: - View
 public struct RepositoryListView: View {
-    let store: StoreOf<RepositoryList>
+    @Bindable var store: StoreOf<RepositoryList>
     
     public init(store: StoreOf<RepositoryList>) {
         self.store = store
     }
     
     public var body: some View {
-        Group {
-            if store.isLoading {
-                ProgressView()
-            } else {
-                List {
-                    ForEach(
-                        store.scope(
-                            state: \.repositoryRows,
-                            action: \.repositoryRows
-                        ),
-                        content: RepositoryRowView.init(store:)
-                    )
+        NavigationStack {
+            Group {
+                if store.isLoading {
+                    ProgressView()
+                } else {
+                    List {
+                        ForEach(
+                            store.scope(
+                                state: \.repositoryRows,
+                                action: \.repositoryRows
+                            ),
+                            content: RepositoryRowView.init(store:)
+                        )
+                    }
                 }
             }
-        }
-        .onAppear {
-            store.send(.onAppear)
+            .onAppear {
+                store.send(.onAppear)
+            }
+            .navigationTitle("Repositories")
+            .searchable(
+                text: $store.query,
+                placement: .navigationBarDrawer,
+                prompt: "Input query"
+            )
         }
     }
 }
